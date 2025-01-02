0bfd2dbedde5a95a3f958ee7c5639adfc1650f79
Samarth Verma <verma.samarth05@gmail.com>
2025-01-02 01:37:25 -0500

    generate word function

---
+++ imposter.py
@@ -0,0 +1,31 @@
+import openai
+from dotenv import load_dotenv
+import sys
+
+load_dotenv()
+
+def main():
+    theme = "Disney Characters"
+    generate_word(theme)
+
+
+
+
+def generate_word(theme):
+    
+    message = f"Generate a word related to the theme. Respond ONLY with the word. For example if the theme is 'Pixar Movies', then a valid word would be 'Cars 2'. "
+    message += f"The theme is: {theme}."
+    print(message)
+
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[ 
+        {"role": "user", "content": message} ]
+    )
+
+    resp = response.choices[0].message.content
+
+    return resp
+
+if __name__ == "__main__":
+    main()
